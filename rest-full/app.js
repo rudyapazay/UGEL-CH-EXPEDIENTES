55c71b980bfd24d9b80b00c53baa9aa2b9c86530
Rudy Apaza <rudyapazay@gmail.com>
2019-03-12 03:45:41 -0500

    commit

---
+++ rest-full/app.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var express = require('express');
+var bodyParser=require('body-parser');
+
+var app = express();
+
+//carga de rutas
+
+//var album_routes = require('./routes/album');
+//var image_routes = require('./routes/image');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// configuracion de cabeceras
+//permitiendo 
+//permite la configuracion de paginas
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','X-API-KEY, Origin, X-Requested-With,Content-Type,Accept,Access-Control-Request-Method');
+    res.header('Access-Control-Allow-Methods','GET,POST, OPTIONS, PUT, DELETE');
+    res.header('Allow','GET,POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
+// rutas base
+//app.use('/api',album_routes);
+//app.use('/api',image_routes);
+
+
+module.exports = app;
---
+++ rest-full/index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var port = process.env.PORT || 3700;
+
+var app=require('./app');
+
+
+var mongoURI= 'mongodb://localhost:27017';
+mongoose.connect('mongodb://localhost:27017/app_ugel',{useNewUrlParser: true}, (err,res)=>{
+    if(err){
+        throw err;
+    }
+    else{
+        console.log("Base de datos funcionando correctamente...");
+
+        app.listen(port, () => {
+            console.log('Servidor funcionando en: '+port);
+        });
+        
+    }
+});
+
+mongoose.set('useFindAndModify', false);
+
+
+
